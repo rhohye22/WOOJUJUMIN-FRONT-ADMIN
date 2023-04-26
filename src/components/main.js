7e3b35ee2cb0195cb67079eb743f3de4b161505f
rhohye22 <nhw666@gmail.com>
2023-04-26 11:44:36 +0900

    4/26

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,19 @@
 import member from "./image/free-icon-group-users-4121044.png";
 import qna from "./image/free-icon-letter-q-7336374.png";
 import calendar from "./image/free-icon-calendar-833593.png";
+import leader from "./image/free-icon-leader-4249109.png";
 
 function Main() {
   return (
     <div className="menucontainer1">
+      <div className="menucontainer2">
+        <a href="/leader-management">
+          <div className="background">
+            <img className="mainmenu" src={leader} alt="My Image" />
+          </div>
+        </a>
+        <p>파티장권한부여</p>
+      </div>
       <div className="menucontainer2">
         <a href="/member-management">
           <div className="background">
